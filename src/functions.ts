ce6443ecad12fd3237ed007809c2cedf33da9df2
Alexis Reese <ymldnn@gmail.com>
2023-05-12 03:17:17 -0500

    deleted unused components

--- src/functions.ts
+++
@@ -1,5 +0,0 @@
-import { Toggle as _Toggle } from "./interfaces/interfaces";
-
-export const toggleFunction = ({ changeToggle, current }: _Toggle) => {
-  changeToggle(!current);
-};
--- src/functions/functions.ts
+++
@@ -1,6 +0,0 @@
-import { useMutation } from "@apollo/client";
-
-/* Function to toggle state */
-export const toggleFunction = (prop: boolean): boolean => {
-  return !prop;
-};
