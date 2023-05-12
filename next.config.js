4bd87d9e02cd17ee6f61f0da0660ee3fcaa49953
Alexis Reese <ymldnn@gmail.com>
2023-05-12 03:27:18 -0500

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -2,26 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       // destination: "http://localhost:4000/:path*",
-  //       destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
-// module.exports = {
-//   eslint: {
-//     // Warning: This allows production builds to successfully complete even if
-//     // your project has ESLint errors.
-//     ignoreDuringBuilds: true,
-//   },
-// };
